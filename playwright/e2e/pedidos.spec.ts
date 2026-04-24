4ff67c63e342ac55dec625857519f170309c3ab6
Paulo Cesar Andrades <pcnandrades@gmail.com>
2026-04-24 10:52:21 -0300

    iniciando a automação dos testes de consulta de pedido aprovado

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -1,25 +1,25 @@
-import { test, expect } from '@playwright/test'
+import { test, expect } from '@playwright/test';
 
 test('deve consultar um pedido aprovado', async ({ page }) => {
-  await page.goto('http://localhost:5173/')
+  await page.goto('http://localhost:5173/');
 
   // Checkpoint
-  await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
+  await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
 
   // Acessa a página de consulta de pedidos
   // a[href="/lookup"]
-  await page.getByRole('link', { name: 'Consultar Pedido' }).click()
+  await page.getByRole('link', { name: 'Consultar Pedido' }).click();
 
   // Checkpoint
-  await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
+  await expect(page.getByRole('heading')).toContainText('Consultar Pedido');
 
-  await page.getByTestId('search-order-id').fill('VLO-9C6DL3')
+  await page.getByTestId('search-order-id').fill('VLO-9C6DL3');
 
-  await page.getByTestId('search-order-button').click()
-  await expect(page.getByTestId('order-result-id')).toBeVisible()
-  await expect(page.getByTestId('order-result-id')).toContainText('VLO-9C6DL3')
+  await page.getByTestId('search-order-button').click();
+  await expect(page.getByTestId('order-result-id')).toBeVisible();
+  await expect(page.getByTestId('order-result-id')).toContainText('VLO-9C6DL3');
+  
+  await expect(page.getByTestId('order-result-status')).toBeVisible();
+  await expect(page.getByTestId('order-result-status')).toContainText('APROVADO');
 
-  await expect(page.getByTestId('order-result-status')).toBeVisible()
-  await expect(page.getByTestId('order-result-status')).toContainText('APROVADO')
-
-})
+});

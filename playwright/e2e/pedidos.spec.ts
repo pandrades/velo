898cbdb2ed0b2de4f37c5178173ecbcdf4b7615c
Paulo Cesar Andrades <pcnandrades@gmail.com>
2026-04-29 09:47:26 -0300

    Adiciona timeout global de 5000ms e atualiza teste de consulta de pedido para usar 'Número do Pedido' e verificar status 'APROVADO'

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -6,18 +6,21 @@ test('deve consultar um pedido aprovado', async ({ page }) => {
   // Arrange
   await page.goto('http://localhost:5173/')
   await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
+
   await page.getByRole('link', { name: 'Consultar Pedido' }).click()
   await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
 
   // Act
-  await page.getByTestId('search-order-id').fill('VLO-9C6DL3')
+  await page.getByRole('textbox', { name: 'Número do Pedido' }).fill('VLO-9C6DL3')
+
   await page.getByTestId('search-order-button').click()
 
+
   // Assert
-  await expect(page.getByTestId('order-result-id')).toBeVisible()
+  await expect(page.getByTestId('order-result-id')).toBeVisible({ timeout: 10000 })
   await expect(page.getByTestId('order-result-id')).toContainText('VLO-9C6DL3')
   
   await expect(page.getByTestId('order-result-status')).toBeVisible()
-  await expect(page.getByTestId('order-result-status')).toContainText('APROVADU')
+  await expect(page.getByTestId('order-result-status')).toContainText('APROVADO')
 
 })

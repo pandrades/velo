5e84154e4653aa9e6ae4abedc4d6451ac1dc99f0
Paulo Cesar Andrades <pcnandrades@gmail.com>
2026-08-17 16:48:47 -0300

    Adiciona novo teste para exibir mensagem de erro quando o pedido não for encontrado e define dados de teste para consulta de pedido aprovado.

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -3,6 +3,10 @@ import { test, expect } from '@playwright/test'
 /// AAA - Arrange, Act, Assert
 
 test('deve consultar um pedido aprovado', async ({ page }) => {
+
+  //Test Data  
+  const orderNumber = 'VLO-9C6DL3'
+
   // Arrange
   await page.goto('http://localhost:5173/')
   await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
@@ -24,4 +28,23 @@ test('deve consultar um pedido aprovado', async ({ page }) => {
   //  await expect(page.getByTestId('order-result-status')).toContainText('APROVADO')
     await page.getByTestId('order-result-status').click()
 
+})
+
+test('deve exibir mensagem de erro quando o pedido não for encontrado', async ({ page }) => {
+  const orderNumber = 'VLO-ABC123'
+
+  await page.goto('http://localhost:5173/')
+  await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
+
+  await page.getByRole('link', { name: 'Consultar Pedido' }).click()
+  await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
+
+  await page.getByRole('textbox', { name: 'Número do Pedido' }).fill('orderNumber')
+  await page.getByRole('button', { name: 'Buscar Pedido' }).click()
+  
+  await expect(page.locator('#root')).toMatchAriaSnapshot(`
+    - img
+    - heading "Pedido não encontrado" [level=3]
+    - paragraph: Verifique o número do pedido e tente novamente
+    `);
 })
